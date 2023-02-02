ea0f2905145ccf9a751a8626cba9d80af9b2ede5
Oliver Bachmann <obachmann@ymail.com>
2023-02-02 23:25:43 +0100

    Removed logs, fixed undo for resize and for adding TOOD change text and better deletion also of character meshes

--- UndoManager.js
+++ UndoManager.js
@@ -6,25 +6,28 @@ class UndoManager {
 
     pushState(state) {
         this.states.push(state);
+        console.log("PUSHED");
     }
 
     undo() {
         console.log("UNDO CALLED");
-        if (this.states.length <= 1) return;
+        console.log(this.states.length);
+        if (this.states.length < 1) return;
+        console.log("UPDATEUI CALLED");
         this.updateUI(this.states[this.states.length - 1]);
         this.states.pop();
     }
 
-    addItem(id, text, itemType) {
-        this.pushState({ type: "add", id, text, itemType });
+    addItem(id, addedIDs) {
+        this.pushState({ type: "add", id, addedIDs });
     }
 
     changeItemText(id, text) {
         this.pushState({ type: "changeText", id, text });
     }
 
-    resizeItem(id, size) {
-        this.pushState({ type: "resize", id, size });
+    resizeItem(id, originalRadius, childAndParentRadiuses) {
+        this.pushState({ type: "resize", id, originalRadius, childAndParentRadiuses });
     }
 
     deleteItem(id, objects) {
